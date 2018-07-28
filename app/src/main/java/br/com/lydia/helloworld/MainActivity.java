3ea1bbbfd6dfdcb0ee8680b61d705e9da06e4e64
Aluno do Inatel <aluno@local.inatel.br>
2018-07-28 17:35:00 -0300

    aula dm114.

---
+++ app/src/main/java/br/com/lydia/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.lydia.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().isEmpty())
+                    textView.setText(editText.getText().toString());
+            }
+        });
+    }
+}
